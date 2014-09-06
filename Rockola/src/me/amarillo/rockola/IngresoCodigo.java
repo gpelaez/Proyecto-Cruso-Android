e607d9b17b3b8e2780ebc14ea41fd37376d74fd6
analu0409 <analu0409@gmail.com>
2014-09-06 11:08:40 -0500

    Limpieza de proyecto

--- Rockola/src/me/amarillo/rockola/IngresoCodigo.java
+++ Rockola/src/me/amarillo/rockola/IngresoCodigo.java
@@ -16,24 +16,8 @@ public class IngresoCodigo extends ActionBarActivity {
 		setContentView(R.layout.activity_ingreso_codigo);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.ingreso_codigo, menu);
-		return true;
-	}
+	
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 	
 	public void ingresarEstablecimiento(View v)
 	{
