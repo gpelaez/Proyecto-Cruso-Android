03707d2f9c3cdae6359b0e5781d55eb95c880ff0
analu0409 <analu0409@gmail.com>
2014-09-06 13:01:38 -0500

    Creación de activities y navigationDrawer

--- Rockola/src/me/amarillo/entidades/DJ.java
+++ Rockola/src/me/amarillo/entidades/DJ.java
@@ -1,5 +1,15 @@
 package me.amarillo.entidades;
 
+import me.amarillo.utiles.AmarilloResponseHandler;
+import android.util.Log;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessCollection;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.async.callback.BackendlessCallback;
+import com.backendless.exceptions.BackendlessFault;
+import com.backendless.persistence.BackendlessDataQuery;
+
 public class DJ {
 	
 	long id;
@@ -31,5 +41,36 @@ public class DJ {
 		this.contrasena = contrasena;
 	}
 	
-	
+	public void guardar() {
+		Backendless.Persistence.save(this,
+				new BackendlessCallback<DJ>() {
+					@Override
+					public void handleResponse(DJ oQuestion) {
+						Log.i("", DJ.class.getSimpleName()
+								+ " creada " + oQuestion.toString());
+					}
+				});
+
+	}
+
+	public static void obtener(int id, final AmarilloResponseHandler response) {
+		String whereClause = "id = " + id;
+		BackendlessDataQuery dataQuery = new BackendlessDataQuery();
+		dataQuery.setWhereClause(whereClause);
+		Backendless.Persistence.of(DJ.class).find(dataQuery,
+				new AsyncCallback<BackendlessCollection<DJ>>() {
+
+			@Override
+			public void handleFault(BackendlessFault arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void handleResponse(
+					BackendlessCollection<DJ> respuesta) {
+				response.callbackDJs(respuesta.getData());
+			}
+		});
+	}
 }
--- Rockola/src/me/amarillo/entidades/Dedicatoria.java
+++ Rockola/src/me/amarillo/entidades/Dedicatoria.java
@@ -1,5 +1,15 @@
 package me.amarillo.entidades;
 
+import me.amarillo.utiles.AmarilloResponseHandler;
+import android.util.Log;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessCollection;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.async.callback.BackendlessCallback;
+import com.backendless.exceptions.BackendlessFault;
+import com.backendless.persistence.BackendlessDataQuery;
+
 public class Dedicatoria {
 
 	long id;
@@ -8,7 +18,7 @@ public class Dedicatoria {
 	String artista;
 	String de;
 	String para;
-	String mensaje;	
+	String mensaje;
 	Boolean enCola;
 
 	public long getId() {
@@ -74,5 +84,37 @@ public class Dedicatoria {
 	public void setEnCola(Boolean enCola) {
 		this.enCola = enCola;
 	}
-	
+
+	public void guardar() {
+		Backendless.Persistence.save(this,
+				new BackendlessCallback<Dedicatoria>() {
+					@Override
+					public void handleResponse(Dedicatoria oQuestion) {
+						Log.i("", Dedicatoria.class.getSimpleName()
+								+ " creada " + oQuestion.toString());
+					}
+				});
+
+	}
+
+	public static void obtener(int id, final AmarilloResponseHandler response) {
+		String whereClause = "id = " + id;
+		BackendlessDataQuery dataQuery = new BackendlessDataQuery();
+		dataQuery.setWhereClause(whereClause);
+		Backendless.Persistence.of(Dedicatoria.class).find(dataQuery,
+				new AsyncCallback<BackendlessCollection<Dedicatoria>>() {
+
+			@Override
+			public void handleFault(BackendlessFault arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void handleResponse(
+					BackendlessCollection<Dedicatoria> dedicatorias) {
+				response.callbackDedicatorias(dedicatorias.getData());
+			}
+		});
+	}
 }
